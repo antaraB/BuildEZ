908b3f32d7b28841351889ac69e4f5b481a8a82e
Antara <abhowmick@ucdavis.edu>
2018-05-17 14:03:21 -0700

    Fixed github links

--- scripts/read_bugswarm.py
+++ scripts/read_bugswarm.py
@@ -12,9 +12,9 @@ pass_sha = jsondata['passed_job']['trigger_sha']
 fail_sha = jsondata['failed_job']['trigger_sha']
 repo = jsondata['repo']
 gitrepolink = "https://www.github.com/"+str(repo)
-gitcomparelink = gitrepolink + "/compare/"+pass_sha+"..."+fail_sha
+gitcomparelink = gitrepolink + "/compare/"+fail_sha+"..."+pass_sha
 
 print("Github link: ",gitrepolink)
-print("Github failed trigger commit: ",fail_sha)
-print("Github passed trigger commit: ",pass_sha)
+print("Github failed trigger commit: ",gitrepolink+"/commit/"+fail_sha)
+print("Github passed trigger commit: ",gitrepolink+"/commit/"+pass_sha)
 print("Github compare link: ",gitcomparelink)
