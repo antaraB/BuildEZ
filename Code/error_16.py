76e1b917a935350a8fc60ddfc639fe26c425aab1
Antara <abhowmick@ucdavis.edu>
2018-06-06 19:23:40 -0700

    Added file for error #16 from dependency errors

---
+++ Code/error_16.py
@@ -0,0 +1,26 @@
+#assuming we've detected that it's this error
+# error: package X not detected
+'''
+PROCEDURE
+read failed log 
+separate grep errors?
+
+
+
+
+'''
+
+
+import re
+
+FAILED_LOG = ""
+GREP_ERROR = ""
+required_package_name = None
+regex_package_not_found = r"(package [\w.]+ does not exist)"
+regex_till_package_not_found = r"(.)+ package [\w.]+ does not exist"
+regex_filename = r"((\/([\w+-]+))+.java):(\[(\d+),(\d+)\])" #gets the java file which needs to be edited
+# group1 is file name (with path), group 3 is just filename (no java). group 5 is line num, group 6 is col num.
+with open(GREP_ERROR) as grep_error_file:
+	grep_error_file.read()
+
+
