1caa86ba7d2cd4403e19220a947a9c94e7646f78
preritauti <preritauti@gmail.com>
2018-06-08 22:14:32 -0700

    Code clean-up

--- Code/could_not_resolve_dependencies.py
+++ Code/could_not_resolve_dependencies.py
@@ -10,6 +10,7 @@ import xml.etree.ElementTree as xml
 
 input = "Failed to execute goal on project projectkorra: Could not resolve dependencies for project com.projectkorra:projectkorra:jar:1.8.2: Could not find artifact org.generallib:GLib:jar:LATEST at specified path /home/travis/build/ProjectKorra/ProjectKorra/lib/GL.jar -> [Help 1]"
 input2 = "[ERROR] Failed to execute goal on project user-manager: Could not resolve dependencies for project com.peterphi.user-manager:user-manager:war:9.0.2-SNAPSHOT: Could not find artifact AzureLogAppender:AzureLogAppender:jar:1.0 in central (http://repo.maven.apache.org/maven2)"
+namespaces = {'xmlns' : 'http://maven.apache.org/POM/4.0.0'}
 
 
 #STORING PATHS TO ALL POM.XML FILES IN AN ARRAY
@@ -19,20 +20,6 @@ def find_all_pom_files(name, path):
 		if name in files:
 			poms.append(os.path.join(root, name))
 	return poms
-
-
-#OPENING EACH POM FILE TO LOOK FOR THE ONE WITH THE TAG AND REMOVE SNAPSHOT
-# def find_the_correct_pom_file(patharray):
-# 	tag_description = re.search(r"\:([\w-]+)\:ja	r\:([\d\.]+-SNAPSHOT)",input_error).groups()[0]
-# 	snapshot = re.search(r"\:([\w-]+)\:jar\:([\d\.]+-SNAPSHOT)",input_error).groups()[1]
-# 	for filepath in patharray:
-# 		with open(filepath, w+) as filename:
-# 			for line in filename:
-# 				if tag_description in line and snapshot in line:
-# 					line = re.sub(r"([\d\.]+)(-SNAPSHOT)",r"\1",line)
-# 				newfile.append(line)
-# 			filename.write(newfile)
-
 
 def main():
 	print "Inside main function"
@@ -50,36 +37,33 @@ def main():
 		print grouped_output.group(4)
 		print grouped_output.group(5)
 
+		artifact = grouped_output.group(2).split(":")
+		print artifact
+
 		#Find the file which needs to be opned.
-		poms = find_all_pom_files("pom.xml","/home/travis/build/failed")
+		poms = find_all_pom_files("pom.xml","/home/prerit/Spring2018/ECS260/Project/BuildEZ/Code")
+		#poms = find_all_pom_files("pom.xml","/home/travis/build/failed")
 		print "POMSSSS"
 		print poms
 
-		pomFile = xml.parse('/home/prerit/Spring2018/ECS260/Project/BuildEZ/Code/pom.xml')
-		root = pomFile.getroot()
-		namespaces = {'xmlns' : 'http://maven.apache.org/POM/4.0.0'}
+		for filepath in poms:
+			pomFile = xml.parse(filepath)
+			root = pomFile.getroot()
 
 
-		deps = root.findall(".//xmlns:dependency", namespaces=namespaces)
-		for d in deps:
-			artifactId = d.find("xmlns:artifactId", namespaces=namespaces)
-			version = d.find("xmlns:version", namespaces=namespaces)
-			systemPath = d.find("xmlns:systemPath", namespaces=namespaces)
-			if(artifactId.text == "GLib"):
-				print systemPath.text
-				path = systemPath.text
-				systemPath.text = systemPath.text.replace("GL.jar", "GLib.jar")
-				print systemPath.text
+			deps = root.findall(".//xmlns:dependency", namespaces=namespaces)
+			for d in deps:
+				artifactId = d.find("xmlns:artifactId", namespaces=namespaces)
+				version = d.find("xmlns:version", namespaces=namespaces)
+				systemPath = d.find("xmlns:systemPath", namespaces=namespaces)
+				if(artifactId.text == artifact[1]):
+					print systemPath.text
+					path = systemPath.text
+					systemPath.text = systemPath.text.replace(grouped_output.group(5), artifact[1] + "." + artifact[2])
+					print systemPath.text
 
-		pomFile.write('pom_new.xml')
+			pomFile.write(filepath)
 
-		#mappings = getMappings(root)
-		#print mappings
-
-		#Replace the error of the file
-		#find_the_correct_pom_file(poms)
-
-		#Check if the fix works.
 	else :
 		print "We are inside else statement"
 		regex_whole_damn_thing = r".+ (Could not find artifact ([\w\.\:]+) ([\w\.+-]+))"
