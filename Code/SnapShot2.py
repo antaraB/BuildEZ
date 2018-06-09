fa99c58cba6d19e872f0444a3b5f55bd783afaa0
Nehal <nehalagrawal.nmims@gmail.com>
2018-06-09 14:33:57 -0700

    Modified snapshot code. SnapShot uses files, SnapShot2 uses xml parser

--- Code/SnapShot2.py
+++ Code/SnapShot2.py
@@ -9,44 +9,38 @@
 
 #-----------------------------------------------------------------------------------------------------------------------------------
 
-#name - pom.xml and path - /home/travis/build/failed
+#name - pom.xml
+#path - /home/travis/build/failed
+
 
 import re
 import os
+import xml.etree.ElementTree as xml
+import subprocess
 
-input_error = None
+input_error = "Failed to collect dependencies at com.joyent.http-signature:google-http-client-signature:jar:2.0.0-SNAPSHOT"
+input_error2 = "Failed to collect dependencies at com.google.guava:guava:jar:20.0-SNAPSHOT"
 
 #STORING PATHS TO ALL POM.XML FILES IN AN ARRAY
 def find_all_pom_files(name, path):
-	poms = []
-	for root, dirs, files in os.walk(path):
-		if name in files:
-			poms.append(os.path.join(root, name))
-	return poms
+        #print "Inside find_all_pom_files"
+        poms = []
+        for root, dirs, files in os.walk(path):
+                if name in files:
+                        poms.append(os.path.join(root, name))
+        return poms
 
 #OPENING EACH POM FILE TO LOOK FOR THE ONE WITH THE TAG AND REMOVE SNAPSHOT
-def find_the_correct_pom_file(patharray):
-	tag_description = re.search(r"\:([\w-]+)\:jar\:([\d\.]+-SNAPSHOT)",input_error).groups()[0]
-	snapshot = re.search(r"\:([\w-]+)\:jar\:([\d\.]+-SNAPSHOT)",input_error).groups()[1]
-	for filepath in patharray:
-		newfile = []
-		with open(filepath, "w+") as filename:
-			for line in filename.readlines():
-				if tag_description in line and snapshot in line:
-					line = re.sub(r"([\d\.]+)(-SNAPSHOT)",r"\1",line)
-				newfile.append(line)
-			for line in newfile:
-                                filename.write(line)
-'''
 def find_the_correct_pom_file(patharray):
         #print "Inside find_the_correct_pom_file"
         tag_description = re.search(r"\:([\w-]+)\:jar\:([\d\.]+-SNAPSHOT)",input_error).groups()[0]
         snapshot = re.search(r"\:([\w-]+)\:jar\:([\d\.]+-SNAPSHOT)",input_error).groups()[1]
         #print tag_description, snapshot, patharray
         for filepath in patharray:
+                '''
                 newfile = []
                 #print type(filepath)
-                with open('%s' % filepath, 'r') as filename:
+		with open('%s' % filepath, 'r') as filename:
                         if tag_description in filename.read():
                         #print "------------------------------------------------------------------------"
                         #print filename.readline()
@@ -62,16 +56,40 @@ def find_the_correct_pom_file(patharray):
                                 for line in newfile:
                                         filename.write(line)
                                 #print filename.readlines()
+                        '''
+                with open('%s' % filepath, 'r') as filename:
+                        if tag_description in filename.read():
+                                pomfile = xml.parse(filepath)
+                                root = pomfile.getroot()
+                                namespaces = {'xmlns' : 'http://maven.apache.org/POM/4.0.0'}
+                                properties = root.find(".//xmlns:properties", namespaces = namespaces)
+                                snap = properties.find(".//xmlns:dependency." + tag_description + ".version", namespaces = namespaces)
+                                #print snap.text
+                                snap.text = re.sub(r"(.+)(-SNAPSHOT)",r"\1",snap.text)
+																pomfile.write(filepath)
+
+#REBUILDING PROJECT AFTER FIXING POM FILE
+def rebuild_using_new_pom():
+	result = subprocess.call('/usr/local/bin/run_failed.sh')
+	#print result
+	if result == 0:
+		print "Build passed!! WOOOHOOOOO!"
+	elif result == 2:
+		print "MAA KI AANKH!"
 
 
-'''
 
 #MAIN FUNCTION
 def main():
 	name = "pom.xml"
-	path = "/home/travis/build/failed"
-	poms = find_all_pom_files(name, path)
-	find_the_correct_pom_file(poms)
-if __name__=="__main__":
-	main()
+  path = "/home/travis/build/failed"
+  poms = find_all_pom_files(name, path)
+  #print poms
+  find_the_correct_pom_file(poms)
+	rebuild_using_new_pom()	
+	if __name__=="__main__":
+		main()
+
+
+
 
