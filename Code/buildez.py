125b29c72e40dc540330a00e690dfcc1b866e6b8
Antara <abhowmick@ucdavis.edu>
2018-06-11 16:03:00 -0700

    Added more structure to code, still untested

--- Code/buildez.py
+++ Code/buildez.py
@@ -2,32 +2,43 @@
 from __future__ import print_function
 import re
 import argparse
-from package_does_not_exist import get_info_from_error_message, fix_error_package_dne
+from package_does_not_exist import get_info_from_error_message, fix_error_package_dne, modify_to_failed
 import check_build
 
 
 def main(to_print):
 	subprocess.call('./scripts/diffandgrep.sh')
-	build_pass = False
-	with open('grep_errors.txt') as grep_error_file:
-		for line in grep_error_file.read():
+	build_pass = False # whether build has passed or not
+	build_message = None # any message you want to print with build passed (usually error name/ brief summary)
+	try:
+		with open('grep_errors.txt') as grep_error_file:
+			for line in grep_error_file.read():
 
-			# ERROR: package <package-name> does not exist
-			filepath, required_package_name, line_num = get_info_from_error_message(line, to_print)
-			if filepath and required_package_name:
-				if fix_error_package_dne(modify_to_failed(filepath), package_name, line_num, to_print)>0:
-		            if check_build.main(to_print): #Build Passed!
-		            	build_pass = True
-		            	break
-				else : # Error wasn't fixed
-					if to_print:
-						print("Tried package-does-not-exist, no error detected and no changes made to build")
+				# ERROR: package <package-name> does not exist
+				filepath, package_name, line_num = get_info_from_error_message(grep_error, to_print)
+				if filepath and package_name:
+					if fix_error_package_dne(modify_to_failed(filepath), package_name, line_num, to_print)>0: #Fix worked as expected
+			            if check_build.main(to_print): #Build Passed!
+			            	build_pass = True
+			            	build_message = "Fixed package-does-not-exist error by removing "+package_name+" import statement from "+modify_to_failed(filepath)
+			            	break
+					else : # Error wasn't fixed
+						if to_print:
+							print("Tried package-does-not-exist, no error detected and no changes made to build")
 
+				# ADD OTHER ERRORS HERE
 
-
-
-
-
+	except IOError as e: # couldn't open grep_errors.txt
+        if to_print:
+            print("Couldn't open grep_errors.txt to read (%s)." % e)
+        return -1 # couldn't open file
+	
+	if build_pass:
+		print("Build passed : ", build_message)
+		return 1
+	else :
+		print("Build failed: Error couldn't be fixed using the defined dependency-error fixes")
+		return -1
 
 
 if __name__ == "__main__":
