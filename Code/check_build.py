e94e1517aa2e413f0e7e05228993759b57ba34c8
Antara <abhowmick@ucdavis.edu>
2018-06-11 14:05:46 -0700

    Added some options to check_build

--- Code/check_build.py
+++ Code/check_build.py
@@ -1,8 +1,30 @@
+#!/usr/bin/env python
+from __future__ import print_function
 import subprocess
+import argparse
+try:
+    from subprocess import DEVNULL
+except ImportError:
+    import os
+    DEVNULL = open(os.devnull, 'wb')
 
-result = subprocess.call('/usr/local/bin/run_failed.sh')
-print result
-if result == 0:
-	print "Build passed!! WOOOHOOOOO!"
-elif result == 2:
-	print "MAA KI AANKH!"
+
+def main(to_print):
+
+    if to_print:
+        result = subprocess.call('/usr/local/bin/run_failed.sh')
+    else :  
+        result = subprocess.call('/usr/local/bin/run_failed.sh', stdout=DEVNULL, stderr=subprocess.STDOUT)
+    
+    if result == 0:
+        print("Build Passed")
+    elif result == 2:
+        print("Build Failed")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v','--verbose', action='store_true', default=False, help=" Will not suppress output from build")
+    args = parser.parse_args()
+    to_print = args.verbose
+    main(to_print)
